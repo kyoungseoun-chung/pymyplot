af0a72895bced380c6a642703e69efe17539f6ef
Kyoungseoun Chung <kyoungseoun.chung@gmail.com>
2023-03-09 11:10:24 +0100

    test: :white_check_mark: revised test file

--- tests/test_presets.py
+++ tests/test_presets.py
@@ -2,16 +2,14 @@
 """Test font module."""
 import pytest
 
-from pymyplot.aip import Font as AIPFont
-from pymyplot.aip import Line as AIPLine
-from pymyplot.aip import Marker as AIPMarker
-from pymyplot.basic import Font as BasicFont
-from pymyplot.basic import Line as BasicLine
-from pymyplot.basic import Marker as BasicMarker
+from pymyplot import myplt
 
 
 def test_basic_setup() -> None:
     from pymyplot.scaling import SIZE_SCALE, WIDTH_SCALE, FONT_WEIGHT
+    from pymyplot.basic import Font as BasicFont
+    from pymyplot.basic import Line as BasicLine
+    from pymyplot.basic import Marker as BasicMarker
 
     assert BasicFont.default_size == 16.0
     assert BasicMarker.default_size == 16.0
@@ -34,9 +32,19 @@ def test_basic_setup() -> None:
     for k, v in FONT_WEIGHT.items():
         assert pytest.approx(BasicFont.weight(k)) == v
 
+    assert myplt.rcParams["legend.fontsize"] == BasicFont.size("text-lg")
+    assert myplt.rcParams["figure.titlesize"] == BasicFont.size("text-2xl")
+    assert myplt.rcParams["axes.labelsize"] == BasicFont.size("text-base")
+    assert myplt.rcParams["xtick.labelsize"] == BasicFont.size("text-xs")
+    assert myplt.rcParams["ytick.labelsize"] == BasicFont.size("text-sm")
+
 
 def test_aip_setup() -> None:
+
     from pymyplot.scaling import SIZE_SCALE, WIDTH_SCALE
+    from pymyplot.aip import Font as AIPFont
+    from pymyplot.aip import Line as AIPLine
+    from pymyplot.aip import Marker as AIPMarker
 
     assert AIPFont.default_size == 12.0
     assert AIPMarker.default_size == 10.0
@@ -50,3 +58,9 @@ def test_aip_setup() -> None:
 
     for k, v in WIDTH_SCALE.items():
         assert pytest.approx(AIPLine.width(k)) == v * AIPLine.default_linewidth
+
+    assert myplt.rcParams["legend.fontsize"] == AIPFont.size("text-base")
+    assert myplt.rcParams["figure.titlesize"] == AIPFont.size("text-2xl")
+    assert myplt.rcParams["axes.labelsize"] == AIPFont.size("text-base")
+    assert myplt.rcParams["xtick.labelsize"] == AIPFont.size("text-sm")
+    assert myplt.rcParams["ytick.labelsize"] == AIPFont.size("text-sm")
